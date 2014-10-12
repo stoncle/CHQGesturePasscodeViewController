ec838bc7b8f7c2c650c521bce4e94902990ff37c
stoncle <stoncle100@gmail.com>
2014-10-12 15:31:25 +0800

    Initial Commit

---
+++ CHQGesturePasscodeViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CHQGesturePasscodeViewController
+//
+//  Created by 陈鸿强 on 10/12/14.
+//  Copyright (c) 2014 陈鸿强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CHQGesturePasscodeViewController/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CHQGesturePasscodeViewController
+//
+//  Created by 陈鸿强 on 10/12/14.
+//  Copyright (c) 2014 陈鸿强. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
